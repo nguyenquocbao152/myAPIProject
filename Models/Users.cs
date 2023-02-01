429421b962839b4d66d1479b64a2badb548f61f7
nguyenquocbao152 <bao152nguyen@gmail.com>
2023-02-01 14:52:37 +0700

    Add project files.

---
+++ Models/Users.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myAPIProject.Models
+{
+    public class Users
+    {
+        public int UserId { get; set; }
+        public String UserName { get; set; }
+        public String Password { get; set; }
+
+    }
+}
